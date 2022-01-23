1c30245e44587dbe22b059ee9efc18cd150e7945
Tisocco <Leandro.Tisocco@gapac.com>
2022-01-23 19:27:41 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
     <div className="App">
+		<NavBar />
 		<h1>
 			Casa Marthe
 		</h1>
