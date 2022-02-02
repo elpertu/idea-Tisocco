8e957ed4b084ced62fa64edd20306d90528882f5
Tisocco <Leandro.Tisocco@gapac.com>
2022-02-02 15:46:39 -0300

    Promesas y Loading

--- src/components/item.js
+++ src/components/item.js
@@ -1,7 +1,10 @@
 import { Card, Button } from "react-bootstrap";
 import ItemCount from './itemCount';
 
-const Item = ({item}) => {	
+const Item = ({item, setSelectedProducts}) => {
+	
+	const selectedProducts = () => setSelectedProducts(item);
+	
 	return(
 		<Card style={{ width: '18rem' }}>
 			<Card.Header>
@@ -11,6 +14,9 @@ const Item = ({item}) => {
 				<Card.Img variant="top" src={item.src}/>
 				<Card.Subtitle className="mb-2 text-muted">
 					Disponibles:{item.stock}
+				</Card.Subtitle>
+				<Card.Subtitle>
+					{item.descripcion}
 				</Card.Subtitle>
 			</Card.Body>
 			<ItemCount stock={item.stock}/>
--- src/components/itemListContainer.js
+++ src/components/itemListContainer.js
@@ -1,18 +1,48 @@
 import Item from "./item";
-const items = [
-	{id: 1,stock: 5, nombre: "Mesa Arco", src:"https://static.wixstatic.com/media/bff52c_eda079378b0e4cd5b677bf6132699049~mv2.jpg/v1/fill/w_1222,h_836,al_c,q_85,usm_0.66_1.00_0.01/bff52c_eda079378b0e4cd5b677bf6132699049~mv2.webp"},
-	{id: 2,stock: 4, nombre: "Mesa Campo", src:"https://static.wixstatic.com/media/bff52c_221d544f82224dbc95419d8bf4beae01~mv2.jpg/v1/fill/w_1254,h_836,al_c,q_85,usm_0.66_1.00_0.01/bff52c_221d544f82224dbc95419d8bf4beae01~mv2.webp"},
-	{id: 3,stock: 2, nombre: "Sillon Japo", src:"https://static.wixstatic.com/media/bff52c_f1af50a8796b4030812c77ebc243591b~mv2.jpeg/v1/fill/w_836,h_836,al_c,q_85,usm_0.66_1.00_0.01/bff52c_f1af50a8796b4030812c77ebc243591b~mv2.webp"},
-	{id: 4,stock: 10, nombre: "Porta Llaves", src:"https://static.wixstatic.com/media/bff52c_8dffe67154ce489d85f21c9e461bc10c~mv2.jpg/v1/fill/w_836,h_836,al_c,q_85,usm_0.66_1.00_0.01/bff52c_8dffe67154ce489d85f21c9e461bc10c~mv2.webp"},
-];
+import {productsAPI} from "../helpers/promises";
+import React, { useState, useEffect } from 'react';
 
 const ItemListContainer = () => {
+	const [items, setItems] = useState([]);
+	
+	 const [loading, setLoading] = useState(true);
+	 
+	const [errorMessage, setErrorMessage] = useState("")
+	
+	useEffect(() => {
+		getProducts();
+	},[])
+	
+	const getProducts = async () => {
+		try{
+			const result = await productsAPI;
+			setItems(result);
+		}catch(error){
+			console.log(error)
+		}finally{
+			setLoading(false);
+			console.log("finally")
+		}
+	}
+	
+ 	if(loading){
+		return (
+			<div role="region" aria-label="Code Example" class="ReactPlayground-StyledExample-module--cls2--Z1-mo ReactPlayground-StyledExample-module--cls1--2oYwU bs-example ReactPlayground-StyledExample-module--show-code--rW9o2">
+				<div>
+					<div className="d-flex justify-content-around ">
+						<h1>Espere, cargando...</h1>;
+					</div>
+				</div>
+			</div>
+		)
+	} 
+	 
 	return(
 		<div role="region" aria-label="Code Example" class="ReactPlayground-StyledExample-module--cls2--Z1-mo ReactPlayground-StyledExample-module--cls1--2oYwU bs-example ReactPlayground-StyledExample-module--show-code--rW9o2">
 		<div>
 		<div className="d-flex justify-content-around ">
 			{items.map(item =>(
-				<Item item={item}/>
+				<Item item={item} key={item.id}/>
 			))}
 		</div>
 		</div>
